2c423ef48b87cfe191b6f7d995868e0b1b858587
takumiiiiiiiiii <136224249+takumiiiiiiiiii@users.noreply.github.com>
2024-07-28 13:53:39 +0900

    scripts

--- styles/script.js
+++ styles/script.js
@@ -7,3 +7,16 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
+
+$(window).scroll(function(){
+    var top = $(".hero").offset().top; // ターゲットの位置取得
+    var position = top - $(window).height();  // 発火させたい位置
+    if($(window).scrollTop() > position){
+      // 要素が見えたときの動き 
+      $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    }else{
+      // それ以外の動き
+      //$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+      //$("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    }
+  })
