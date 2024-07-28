9634b1a8a9b231c91bb248ff0d6d219cbceac003
takumiiiiiiiiii <136224249+takumiiiiiiiiii@users.noreply.github.com>
2024-07-28 16:40:30 +0900

    animation

--- styles/script.js
+++ styles/script.js
@@ -7,16 +7,54 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
+var awake=true;
+window.onload = function(){
+    // 青の移動
+    var before = document.getElementById('Before');
+    Before.style.width = '70%';
+    // ピンクの移動
+    var after = document.getElementById('After');
+    after.style.width = '100%';
+    // ヒーローテキストの移動
+    var text = document.getElementById('Herotext');
+    text.style.width = '70%';
 
-$(window).scroll(function(){
-    var top = $(".hero").offset().top; // ターゲットの位置取得
-    var position = top - $(window).height();  // 発火させたい位置
-    if($(window).scrollTop() > position){
-      // 要素が見えたときの動き 
-      $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-    }else{
-      // それ以外の動き
-      //$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-      //$("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
     }
-  })
+var item1=true;
+var item2=true;
+var item3=true;
+$(window).scroll(function(){
+  
+        var top = $("#contents1").offset().top; // ターゲットの位置取得
+        var position = top - $(window).height();  // 発火させたい位置
+        if($(window).scrollTop() > position&&item1==true){
+          // 要素が見えたときの動き 
+          console.log("fafafa")
+          $("#contents1").css({ 'left' : '0px'  } );
+          item1=false;
+        }else{
+          // それ以外の動き
+        }
+
+        var top = $("#contents2").offset().top; // ターゲットの位置取得
+        var position = top - $(window).height();  // 発火させたい位置
+        if($(window).scrollTop() > position&&item2==true){
+          // 要素が見えたときの動き 
+          console.log("fafafa")
+          $("#contents2").css({ 'right' : '0px'} );
+          item2=false;
+        }else{
+          // それ以外の動き
+        }
+
+        var top = $("#contents3").offset().top; // ターゲットの位置取得
+        var position = top - $(window).height();  // 発火させたい位置
+        if($(window).scrollTop() > position&&item3==true){
+          // 要素が見えたときの動き 
+          console.log("fafafa")
+          $("#contents3").css({ 'left' : '0px'  } );
+          item3=false;
+        }else{
+          // それ以外の動き
+        }
+      })
